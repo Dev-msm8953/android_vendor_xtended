Deepak5310/platform_vendor_aosp

kernel: allow custom clang support for CLANG_PREBUILTS
Signed-off-by: Dakkshesh07 <dakkshesh5@gmail.com>
Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

config/BoardConfigKernel.mk
@@ -51,8 +51,21 @@ else
 KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
 endif
 
-CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-r416183b
-GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86
+ifneq ($(TARGET_CLANG_PREBUILTS_VERSION),)
+    ifeq ($(TARGET_CLANG_PREBUILTS_VERSION),latest)
+        # Set the latest version of clang
+        CLANG_PREBUILTS_VERSION := $(shell ls -d $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-r* | xargs -n 1 basename | tail -1)
+    else
+        # Find the clang-* directory containing the specified version
+        CLANG_PREBUILTS_VERSION := clang-$(TARGET_CLANG_PREBUILTS_VERSION)
+    endif
+else
+    # Use the default version of clang if TARGET_CLANG_PREBUILTS_VERSION hasn't been set by the device config
+    CLANG_PREBUILTS_VERSION := clang-r416183b1
+endif
+
+CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(CLANG_PREBUILTS_VERSION)
+GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)
 # arm64 toolchain
 KERNEL_TOOLCHAIN_arm64 := $(GCC_PREBUILTS)/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX_arm64 := aarch64-linux-android-

Dev-msm8953/android_vendor_xtended

Change-Id: 7c8d4a0e4cffdc6c757e1e4e3924632d5c1f9fa8
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
Signed-off-by: TogoFire <italomellopereira@gmail.com>

config/BoardConfigKernel.mk
@@ -67,7 +67,22 @@ KERNEL_VERSION := $(shell grep "^VERSION = " $(TARGET_KERNEL_SOURCE)/Makefile | 
 KERNEL_PATCHLEVEL := $(shell grep "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
 TARGET_KERNEL_VERSION ?= $(shell echo $(KERNEL_VERSION)"."$(KERNEL_PATCHLEVEL))
 
-CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/clang-r450784d
+TARGET_KERNEL_HEADERS ?= $(TARGET_KERNEL_SOURCE)
+
+ifneq ($(TARGET_CLANG_PREBUILTS_VERSION),)
+    ifeq ($(TARGET_CLANG_PREBUILTS_VERSION),latest)
+        # Set the latest version of clang
+        CLANG_PREBUILTS_VERSION := $(shell ls -d $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-r* | xargs -n 1 basename | tail -1)
+    else
+        # Find the clang-* directory containing the specified version
+        CLANG_PREBUILTS_VERSION := clang-$(TARGET_CLANG_PREBUILTS_VERSION)
+    endif
+else
+    # Use the default version of clang if TARGET_CLANG_PREBUILTS_VERSION hasn't been set by the device config
+    CLANG_PREBUILTS_VERSION := clang-r450784d
+endif
+
+CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(CLANG_PREBUILTS_VERSION)
 GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)
 # arm64 toolchain
 KERNEL_TOOLCHAIN_arm64 := $(GCC_PREBUILTS)/aarch64/aarch64-linux-android-4.9/bin